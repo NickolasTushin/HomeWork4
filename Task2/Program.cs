065ead64fdc9a4d89f3e0efb244b7d5264422f3e
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-09 18:44:29 +0300

    UPD Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+
+sum += n % 10;
+n = n/10 ;
+ 
+}
+ Console.WriteLine($" Сумма всех цифр в введенном числе  = {sum}");
+
