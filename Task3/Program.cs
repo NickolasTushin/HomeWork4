b8158855cbf6a34d9cff00b1631d13e01b0d0b60
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-09 21:50:27 +0300

    UPD Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 29: Напишите программу, которая спрашивает у пользователя кол-во элементов и спршивает сами элементы. 
+//После записи элементов в массив, необходимо его вывести на экран.
+//8
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[]arr = new int[n];
+void FillArray(int []arr)
+{
+    int length = arr.Length;       // Размер массива
+    int i = 0;
+while (i < length)
+{
+ Console.WriteLine("Введите элемент массива:  ");
+ arr[i] = Convert.ToInt32(Console.ReadLine()); 
+ i++;
+}
+}
+void PrintArray(int []arr)
+{
+int count = arr.Length;
+int p = 0;
+while (p < count)
+{ 
+
+Console.Write($"[{arr[p]}] ");
+p++;
+}
+}
+FillArray(arr);
+PrintArray(arr);
