58987297701084ad5ef17ebaa4e325a28a9012a1
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-09 17:56:27 +0300

    UPD Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,19 +2,17 @@
 //(A и B) и возводит число A в натуральную степень B.
 //Нельзя использовать функцию Math.Pow
   
- //число, которое нужно возвести в степень
+
 Console.Clear();
 Console.Write("Введите число А: ");
 int a = Convert.ToInt32(Console.ReadLine());                                     
-  //степень числа
+
 Console.Write("Введите число B (степень): ");
 int n =  Convert.ToInt32(Console.ReadLine());      
-    //число, возведенное в степень
-int x=1;
+int x = 1;
 for(int i=0; i<n; i++) 
 {
-
-x*=a;
+x *= a;
 }
 Console.WriteLine($"{a} ^ {n} = {x}");
   
