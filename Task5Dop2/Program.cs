fc6509f4d6a51e7add7203c6b0f8c624ebe88dbc
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-10 16:46:48 +0300

    UPD readme

--- Task5Dop2/Program.cs
+++ Task5Dop2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите четное число больше двух: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 2 || n % 2 !=0)
+{
+    Console.WriteLine("Введите правильное число; ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int[]arr = new int[n];
+n = arr.Length;
+int i = 0;
+int sum = n;
+void FillArray(int []arr)
+{
+    int length = arr.Length;      
+    int i = 0;
+while (i < length)
+{
+    for (i = 0; i <= length; i ++) 
+{
+    arr[i] += i;
+    Console.Write($"  [ {i} ]  ");
+}
+}
+}
+for ( i = 0; i < n; i++ )
+{
+    for (int y = 1; y < n; y++)
+{
+        sum = arr[i] + arr[y];
+        if (sum == arr.Length)
+        {
+          Console.Write($"  [ {sum} ]  ");
+        }
+        {
+        Console.Write($"  [ {sum} ]  ");
+        }
+}
+}
+FillArray(arr);
